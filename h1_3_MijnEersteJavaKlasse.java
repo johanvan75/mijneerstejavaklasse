f98c12e1abdc6917829457ec61f0cc3538d1d88f
johanvan75 <johanvan75@hotmail.com>
2016-09-29 10:07:14 +0200

    Delete h1_3_MijnEersteJavaKlasse.java

--- h1_3_MijnEersteJavaKlasse.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by johan on 15-9-2016.
- */
-public class h1_3_MijnEersteJavaKlasse {
-
-    public static void main(String[] args) {
-        System.out.println("Dit is mijn eerste java klasse!");
-    }
-}
